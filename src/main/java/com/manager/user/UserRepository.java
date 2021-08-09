b714097a7ddfc3caf43431847ab75e7ff4f3385a
Alex Santamaria <santamariaalex146@gmail.com>
2021-08-08 23:37:23 -0500

    CRUD app

---
+++ src/main/java/com/manager/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.manager.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    public Long countById(Integer id);
+}
